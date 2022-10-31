74d9fb954311014d8e6e6fc5268f6213087bbe95
Fabiano Junior <farisiju@gmail.com>
2022-10-30 22:51:46 -0300

    Teste

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,17 @@
 package application;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Program {
 
 	public static void main(String[] args) {
-		System.out.println("Hello world");
-
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);		
+				
+		
+		sc.close();
 	}
 
 }
